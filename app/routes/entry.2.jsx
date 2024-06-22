ab4634d1b6f29de8eb662e110ba3cf9894d49225
yamasho1061 <uchiyamashota9@gmail.com>
2024-06-22 23:19:51 +0900

    entry2 completed

--- app/routes/entry.2.jsx
+++ app/routes/entry.2.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+
 export default function Entry2() {
+  const [memos, setMemos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addMemo = () => {
+    const newMemos = memos.slice();
+    newMemos.push(input);
+    setMemos(newMemos);
+    setInput("");
+  };
+
   return (
-    <>
-      <h1>エントリー2</h1>
-      <div>エントリー2の内容</div>
-    </>
+    <div className="App">
+      <h1>ごく簡単なメモアプリ</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="メモを入力してください"
+      />
+      <button onClick={addMemo}>追加</button>
+      <ul>
+        {memos.map((note, index) => (
+          <li key={index}>{note}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
